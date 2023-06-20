2bef26474d8ca6a2a8f662126c22379cba59e9b9
Ahmed <ahmedessam121026@icloud.com>
2023-06-19 22:37:09 -0700

    ljkj

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char = ch
+
+	putchar (ch);
+
+	return (0);
+}
