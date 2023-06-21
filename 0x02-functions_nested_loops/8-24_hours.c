30ad5d793eebfc2b6944ac268581df0e9d8fbcef
Ahmed <ahmedessam121026@icloud.com>
2023-06-20 18:54:23 -0700

    dgfb

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,11 +12,11 @@ void jack_bauer(void)
 	{
 		for (min = 0; min <= 59; min++)
 		{
-			_putchsar((hr / 10) + 48);
-			_putchsar((hr % 10) + 48);
-			_putchsar(':');
-			_putchsar((min / 10) + 48);
-			_putchsar((min % 10) + 48);
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
 			_putchar('\n');
 		}
 	}
