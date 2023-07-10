9a13fe135bcf89a60abd11611d29c6a879f2dce0
Ahmed <ahmedessam121026@icloud.com>
2023-07-10 16:00:26 -0700

    xgh

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -39,7 +39,7 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
-		for (l = 0; av[i][j] != '\0'; j++, cmpt++)
+		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
 			s[cmpt] = av[i][j];
 
 		s[cmpt] = '\n';
