808c3a303d6565b975eb3fbe23ce0833903dd8c3
Ahmed <ahmedessam121026@icloud.com>
2023-07-31 18:40:37 -0700

    jhgb

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new = malloc(sizeof(listint_t));
 
-	listint_t *node = *head
+	listint_t *node = *head;
 
 	if (!head || !new)
 		return (NULL);
