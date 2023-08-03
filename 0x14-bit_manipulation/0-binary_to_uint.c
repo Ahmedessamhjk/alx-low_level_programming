2310095563fa7da685475043b1663082802b0bbc
Ahmed <ahmedessam121026@icloud.com>
2023-08-03 15:25:48 -0700

    asx

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,7 +8,7 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int binary = 0;
+	unsigned int num = 0;
 	int a;
 
 	if (!b)
@@ -17,8 +17,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[a] < '0' || b[a] > '1')
 			return (0);
-		binary = binary * 2 + (b[a] - '0');
+		num = num * 2 + (b[a] - '0');
 	}
 
-	return (binary);
+	return (num);
 }
