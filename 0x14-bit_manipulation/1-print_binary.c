f2eb6239f0f9c6c50a15b2f004be7437741cd363
Ahmed <ahmedessam121026@icloud.com>
2023-08-03 16:57:00 -0700

    zdfv

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,13 +10,16 @@ void print_binary(unsigned long int n)
 {
 	int a = sizeof(n) * 8, c = 0;
 
-		if (c & 1)
+	while (a)
+	{
+		if (n & 1)
 		{
 			_putchar('1');
 			c++;
 		}
 		else if (c)
 			_putchar('0');
+	}
 	if (!c)
 		_putchar('0');
 }
