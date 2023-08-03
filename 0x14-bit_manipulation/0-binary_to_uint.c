b628409c669b8cf7059ed45f2d4e1b7b2f9e36d5
Ahmed <ahmedessam121026@icloud.com>
2023-08-03 15:20:39 -0700

    kjh

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,16 +12,11 @@ unsigned int binary_to_uint(const char *b)
 	int a;
 
 	if (!b)
-	{
 		return (0);
-	}
-
 	for (a = 0; b[a]; a++)
 	{
 		if (b[a] < '0' || b[a] > '1')
-		{
 			return (0);
-		}
 		sum = sum * 2 + (b[a] - '0');
 	}
 
