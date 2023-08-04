47a72c551e0e4f0caf1e090b47f73901ad78525a
Ahmed <ahmedessam121026@icloud.com>
2023-08-03 18:33:24 -0700

    fsdz

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - clears the bit at a given index
+ * @n: index number
+ * @: bit number
+ *
+ * Return: 1 on success, -1 on failure
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	if (*n & 1L << index)
+		*n ^= 1L << index;
+	return (1);
+}
